3fd04236dae1557e7a1201e35826c8d7ad5f6fd7
Shawky Gamal <138508345+shawky-gamal-22@users.noreply.github.com>
2023-08-11 16:23:27 +0300

    Add files via upload

---
+++ Queue/Arraybased/queue.h
@@ -0,0 +1,22 @@
+#ifndef QUEUE_H_INCLUDED
+#define QUEUE_H_INCLUDED
+#define QueueEntry char
+#define MAXQUEUE 100
+typedef struct queuee {
+    int frontt ;
+    int rear ;
+    int sizee ;
+    QueueEntry entry [MAXQUEUE] ;
+
+} Queue ;
+
+#endif // QUEUE_H_INCLUDED
+
+void Append (QueueEntry, Queue *);
+void Serve(QueueEntry *, Queue *);
+int QueueEmpty(Queue *);
+int QueueFull(Queue *);
+void CreateQueue(Queue *);
+int QueueSize(Queue *);
+void ClearQueue	(Queue *);
+void TraverseQueue	(Queue *, void (*)(QueueEntry));
